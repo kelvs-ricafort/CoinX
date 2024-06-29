074fa54dc641be189e21f6531e38cfe38db2f0f1
Kelvin Ricafort <kelvs.r@icloud.com>
2024-06-29 16:09:03 +0800

    Initial Commit

---
+++ CoinX/CoinXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinXApp.swift
+//  CoinX
+//
+//  Created by Kelvin on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
